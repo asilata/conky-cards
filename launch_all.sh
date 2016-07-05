3f8d04e2c3a2cf1161ba77223f6ea65659cbe18c
Asilata Bapat <asilata@gmail.com>
2016-07-05 10:51:25 -0400

    Updated run paths.

--- launch_all.sh
+++ launch_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sleep 10
-conky -dq -c INSERT_PATH/time_rc
-conky -dq -c INSERT_PATH/system_rc
-conky -dq -c INSERT_PATH/processes_rc
-conky -dq -c INSERT_PATH/memory_rc
-conky -dq -c INSERT_PATH/filesystem_rc
-conky -dq -c INSERT_PATH/network_rc
-conky -dq -c INSERT_PATH/fortune_rc
-conky -dq -c INSERT_PATH/mediaplayer_rc
+conky -dq -c ~/opt/conky-cards/time_rc
+conky -dq -c ~/opt/conky-cards/system_rc
+conky -dq -c ~/opt/conky-cards/processes_rc
+conky -dq -c ~/opt/conky-cards/memory_rc
+conky -dq -c ~/opt/conky-cards/filesystem_rc
+conky -dq -c ~/opt/conky-cards/network_rc
+conky -dq -c ~/opt/conky-cards/fortune_rc
+conky -dq -c ~/opt/conky-cards/mediaplayer_rc
